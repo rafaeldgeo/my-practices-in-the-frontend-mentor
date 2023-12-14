03251f8b5dcaa6cc86ef2a3ee92214072cd70a59
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2023-12-14 13:57:43 -0300

    Update script.js

--- newbie/article-preview-component-master/script.js
+++ newbie/article-preview-component-master/script.js
@@ -1,9 +1,9 @@
 "use strict"
 
 const articleDivShare = document.querySelector(".article__div-share");
+const articleBtn = document.querySelector(".article__btn");
 
-const share = function() {  
-  
+const share = function() {
   const articleDivUser = document.querySelector(".article__div-user");
   const articleDivMidias = document.querySelector(".article__div-midias");
   const articleDivFooter = document.querySelector(".article__div-footer");
@@ -15,10 +15,7 @@ const share = function() {
   articleDivFooter.classList.toggle("article__div-footer--changecolor");
   articleBtn.classList.toggle("article__btn--dark");
   articleSvgArrow.classList.toggle("article__svg-arrow--white");
+
 }
 
 articleDivShare.addEventListener("click", share);
-
-
-
-
