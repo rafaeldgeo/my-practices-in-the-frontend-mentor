41daa92b625ec6326390032dd59d97f26ce2ca5a
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2023-12-24 15:52:45 -0300

    layout finalizado

--- junior/notifications-page-main/notification-element.js
+++ junior/notifications-page-main/notification-element.js
@@ -31,37 +31,59 @@ function setUserImage(element) {
     userImage.setAttribute("width", 90);
     userImage.setAttribute("height", 90);
     userImage.setAttribute("alt", "User Photo");
-    const image = element.getAttribute("data-image");
-    userImage.src = image;
-    divUserImgWrapper.appendChild(userImage); 
-  
+    const image = element.getAttribute("data-img-user");
+    userImage.src = image; 
+
+    divUserImgWrapper.appendChild(userImage);  
 } 
+
 
 function setTypeNotification(element) {
 
     const shadow = element.shadowRoot;
     const spanTypeNotification = shadow.querySelector(".notification-item__type-notification");
+    const link = shadow.querySelector(".notification-item__link");
     const typeNotification = element.getAttribute("data-type-notification");
     let messageAction = "";
 
      switch (typeNotification) {
         case "reacted":
             messageAction = "reacted to your recent post ";
+            link.classList.add("notification-item__link--post");
             break;
         case "followed":
             messageAction = "followed you";
             break;
         case "joined-group":
             messageAction = "has joined your group ";
+            link.classList.add("notification-item__link--group");
             break;
         case "left-group":
             messageAction = "left the group ";
+            link.classList.add("notification-item__link--group");
             break;
         case "commented":
             messageAction = "commented on your picture";
+            const divPictureWrapper = shadow.querySelector(".notification-item__your-picture-wrapper");
+            const pictureImage = document.createElement("img");
+            pictureImage.classList.add("notification-item__picture-img");
+            pictureImage.setAttribute("width", 90);
+            pictureImage.setAttribute("height", 90);
+            pictureImage.setAttribute("alt", "");
+            pictureImage.setAttribute("tabindex", "0");
+            const image = element.getAttribute("data-img-picture");
+            pictureImage.src = image; 
+            divPictureWrapper.appendChild(pictureImage);  
             break;
         case "sent-message":
             messageAction = "sent you a private message ";
+            const divMessageWrapper = shadow.querySelector(".notification-item__message-wrapper");
+            const privativeMessage = document.createElement("p");
+            privativeMessage.classList.add("notification-item__privative-message");
+            const slotPrivativeMessage = document.createElement("slot");
+            slotPrivativeMessage.setAttribute("name", "privative-message");
+            privativeMessage.appendChild(slotPrivativeMessage);
+            divMessageWrapper.appendChild(privativeMessage);
             break;
         default:
             messageAction = "define a message.";            
