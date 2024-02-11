34c6660411d4118a03ff57460b502c4e60ee5212
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-02-11 16:39:06 -0300

    desafio finalizado

--- junior/sunnyside-agency-landing-page-main/index.js
+++ junior/sunnyside-agency-landing-page-main/index.js
@@ -1,7 +1,5 @@
 "use strict";
 
 import AttributionFooter from "./attribution-footer-element.js";
-import MenuElement from "./menu-element.js";
 
 customElements.define("attribution-footer", AttributionFooter);
-customElements.define("menu-element", MenuElement);
