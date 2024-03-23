1299d27821272e670f3cab3813169f0079049e69
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-03-22 21:08:57 -0300

    desafio finalizado

--- junior/newsletter-sign-up-with-success-message-main/thanks-get-email.js
+++ junior/newsletter-sign-up-with-success-message-main/thanks-get-email.js
@@ -1,5 +1,13 @@
 "use strict";
 
-const t =  window.location.href;
+const getUrl = window.location.href.trim();
+const position = getUrl.indexOf("=");
+const emailSliced = getUrl.slice(position+1);
+const email = emailSliced.replace("%40", "@");
+ 
+const sucessMensageEmail = document.querySelector(".sucess-mensage__email");
+sucessMensageEmail.innerText = email;
+  
 
-console.log(t);
+
+
