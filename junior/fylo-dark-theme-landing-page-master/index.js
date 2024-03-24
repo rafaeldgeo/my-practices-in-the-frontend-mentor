b49909c562b1d8a15a6c27351f72bd8abb5e646f
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-03-24 16:31:46 -0300

    iniciando projeto

---
+++ junior/fylo-dark-theme-landing-page-master/index.js
@@ -0,0 +1,5 @@
+import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@3/core/lit-core.min.js';
+import AttributionFooter from "./attribution-footer-element.js";
+
+customElements.define("attribution-footer", AttributionFooter);
+
