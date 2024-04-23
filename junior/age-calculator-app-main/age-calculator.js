1af6125bda4d0032f6c0fcbadd7506b4567886e9
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-04-23 20:49:25 -0300

    inserindo aria-label

--- junior/age-calculator-app-main/age-calculator.js
+++ junior/age-calculator-app-main/age-calculator.js
@@ -227,7 +227,7 @@ export default class AgeCalculatorElement extends LitElement {
              <span class="calculate__error ${this.errorShowMessageYear ? "calculate__error--active": ""}">${this.errorMessageYear}</span>
             </div>
           </div>
-          <button class="calculate__btn" type="submit" @click=${this._submitDate}>
+          <button class="calculate__btn" type="submit" @click=${this._submitDate} aria-label="submit">
             <svg class="calculate__btn-icon" xmlns="http://www.w3.org/2000/svg" width="46" height="44" viewBox="0 0 46 44"><g fill="none" stroke="#FFF" stroke-width="2"><path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44"/></g></svg>
           </button>
         </form>
