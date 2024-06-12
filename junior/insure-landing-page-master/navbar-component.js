ff84c4cd8177a9f8183ce23bdc8d2e6a3213da37
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-06-12 15:44:42 -0300

    desafio finalizado

--- junior/insure-landing-page-master/navbar-component.js
+++ junior/insure-landing-page-master/navbar-component.js
@@ -2,11 +2,52 @@
 
 class NavBar extends React.Component {
 
+    constructor(){
+        super();
+        this.state = {
+            iconmenu: "./images/icon-hamburger.svg",
+            alt: "open menu",
+            typemenu: "navbar__menu-desktop",
+        }
+        this.toogleMenu = this.toogleMenu.bind(this);
+    }
+
+    toogleMenu(){
+        const body = document.querySelector("body");
+        if (this.state.alt === "open menu") {
+            this.setState({
+                iconmenu: "./images/icon-close.svg",
+                alt:"close menu",
+                typemenu: "navbar__menu-mobile",
+            });
+            body.style.overflowY = "hidden";
+        } else {
+            this.setState({
+                iconmenu: "./images/icon-hamburger.svg",
+                alt:"open menu",
+                typemenu: "navbar__menu-desktop",
+            });
+            body.style.overflowY = "auto";
+        }
+       
+        window.addEventListener("resize", () => {
+            const widthView = window.innerWidth;
+            if (widthView > 576 && this.state.alt === "close menu") {
+                this.setState({
+                    iconmenu: "./images/icon-hamburger.svg",
+                    alt:"open menu",
+                    typemenu: "navbar__menu-desktop",
+                });
+                body.style.overflowY = "auto";
+            } 
+          });
+    }
+
     render(){
         return(
             <div className="navbar__menu">
                 <img className="navbar__img-logo" src="./images/logo.svg" alt="logo insure" width="112" height="18"/>
-                <nav className="navbar__menu-desktop">
+                <nav className={this.state.typemenu}>
                     <ul className="navbar__list-menu">
                         <li className="navbar__item-menu"><a className="navbar__item-link" href="#">How we work</a></li>
                         <li className="navbar__item-menu"><a className="navbar__item-link" href="#">Blog</a></li>
@@ -14,8 +55,8 @@ class NavBar extends React.Component {
                         <li className="navbar__item-menu"><a className="navbar__item-link navbar__item-link--button" href="#">View plans</a></li>
                     </ul>
                 </nav>
-                <button className="navbar__btn-menu" type="button" aria-label="open menu">
-                    <img className="navbar__icon-menu" src="./images/icon-hamburger.svg" alt="menu" width="32" height="32"/>
+                <button onClick={this.toogleMenu} className="navbar__btn-menu" type="button" aria-label="open menu">
+                    <img className="navbar__icon-menu" src={this.state.iconmenu} alt={this.state.alt} width="32" height="32"/>
                 </button> 
             </div>
         );
