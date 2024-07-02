82a828e31667d6220b9726d16ab96899e8ba4520
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-07-02 19:24:18 -0300

    desenvolvendo calculadora

--- junior/bmi-calculator/src/components/Calculator.jsx
+++ junior/bmi-calculator/src/components/Calculator.jsx
@@ -1,23 +1,72 @@
-import React, {useId} from "react";
+import React, {useState, useReducer} from "react";
 import "../components/Calculator.css"
 
 export default function Calculator(){
 
-    const id = useId(); 
+    /* const [isOn, setSelected] =  useState(false); */
+
+    const [state, dispatch] = useReducer(reducer, {
+        metric: true,
+        imperial: false
+    });
+
+    const btnRadio = {
+            metric: {
+                border: state.metric ? "none" : "1px solid #5E6E85",
+                backgroundColor: state.metric ? "#CED8FD" : "transparent"
+            },
+            imperial: {
+                border: state.imperial ? "none" : "1px solid #5E6E85",
+                backgroundColor: state.imperial ? "#CED8FD" : "transparent"
+            }
+    };
+
+    const radioSelect = {
+        metric: {
+            backgroundColor: state.metric ? "#345FF6" : "transparent"
+        },
+        imperial: {
+            backgroundColor: state.imperial ? "#345FF6" : "transparent"
+        }
+    }
+
+    function reducer(state, action) {
+        switch(action.type) {
+            case "metric":
+                return {
+                    metric: true,
+                    imperial: false
+                }
+            case "imperial":
+                return {
+                    metric: false,
+                    imperial: true
+                }
+            default:
+        }
+    }
+
+    function handleClickMetric(){
+        dispatch({type: "metric"});
+    }
+
+    function handleClickImperial(){
+        dispatch({type: "imperial"});
+    }
     
     return(
         <div className="calculator">
             <h4 className="calculator__title">Enter your details below</h4>
             <div className="calculator__unit-btns">      
-                <button className="btn" id={id + "metric"}>
-                    <div className="btn__radio btn__radio--on">
-                        <div className="btn__select btn__select--on"></div>
+                <button className="btn" onClick={handleClickMetric} aria-label="Metric">
+                    <div className="btn__radio" style={btnRadio.metric}>
+                        <div className="btn__select" style={radioSelect.metric}></div>
                     </div>
                     <span className="btn__label">Metric</span>
                 </button>
-                <button className="btn" id={id + "imperial"}>
-                    <div className="btn__radio">
-                        <div className="btn__select"></div>
+                <button className="btn" onClick={handleClickImperial} aria-label="Imperial">
+                    <div className="btn__radio" style={btnRadio.imperial}>
+                        <div className="btn__select" style={radioSelect.imperial}></div>
                     </div>
                     <span className="btn__label">Imperial</span>
                 </button>
---
+++ junior/bmi-calculator/src/components/InputMetric.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function InputMetric(){
+
+    return(
+        <div className="inputs">
+            <label htmlFor=""></label>
+            <input type="text" />
+
+        </div>
+    );
+}
