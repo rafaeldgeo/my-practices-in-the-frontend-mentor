2328a119a44f520eff1eb3ae145a382b32943e05
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-07-12 21:28:03 -0300

    input metric finalizado, ajustando layout do calculator

--- junior/bmi-calculator/src/components/Calculator.jsx
+++ junior/bmi-calculator/src/components/Calculator.jsx
@@ -17,6 +17,16 @@ export default function Calculator(){
             <h4 className="calculator__title">Enter your details below</h4>
             <RadioSelectUnit unitSelected={getUnit}/>
             {unitShowed === "metric" ? <InputMetric /> : <InputImperial /> }
+            <div className="calculator__result">
+                <div className="calculator__score">
+                    <h5 className="calculator__subtitle">Your BMI is...</h5>
+                    <span className="calculator__bmi">23.4</span>
+                </div>
+                <div className="calculator__comment-wrapper">
+                    <p className="calculator__comment">Your BMI suggests you're a healthy weight Your ideal weight is between <strong>63.3kgs-85.2kgs</strong></p>
+                </div>
+
+            </div>
         </div>
 
     );
