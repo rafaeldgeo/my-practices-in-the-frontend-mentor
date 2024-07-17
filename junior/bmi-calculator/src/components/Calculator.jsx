255147d5c8c8172ba51f924d8b0f093ed63c95bd
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-07-16 21:38:37 -0300

    calculo do bmi finalizado, falta peso ideal

--- junior/bmi-calculator/src/components/Calculator.jsx
+++ junior/bmi-calculator/src/components/Calculator.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import "../components/Calculator.css"
 import RadioSelectUnit from "./RadioSelectUnit";
 import Form from "./Form";
@@ -6,26 +6,82 @@ import Form from "./Form";
 export default function Calculator(){
 
     const [unitShowed, setUnitShowed] =  useState("metric"); 
-
+    const [resultBmi, setResultBmi] = useState("-");
+    const [classficationBmi, setClassificationBmi ] = useState("...");
+    const [idealWeight, setIdealWeight] = useState({
+        startrange: "...",
+        endrange: "..."
+    });
+    
+    useEffect(()=> {
+        if (resultBmi < 18.5) {
+            setClassificationBmi("underweight");
+        } else if (resultBmi < 25){
+            setClassificationBmi("healthy weight");
+        } else if (resultBmi < 30) { 
+            setClassificationBmi("overweight");
+        } else if (resultBmi > 30) {
+            setClassificationBmi("obese");
+        } else {
+            setClassificationBmi("...");
+        }
+    },[resultBmi]);
+    
     function getUnit(unit){
         setUnitShowed(unit);
+    }
+
+    function getValues(values){
+        if (unitShowed === "metric") {
+            calcMetric(values);
+        } else if (unitShowed === "imperial") {
+            calcImperial(values);
+        } 
+    }
+
+    function calcMetric(values){
+        if (values.height > 0 && values.weight > 0) {
+            let height = Number(values.height) / 100;
+            let weight = Number(values.weight);
+            setResultBmi((weight / (height * height)).toFixed(1));
+            calcIdealWeight(height);
+        } else {
+            setResultBmi("-");
+        }
+    }
+
+    function calcImperial(values){
+        if (values.height > 0 && values.weight > 0 && values.heightin !== "" && values.weightlbs !== "" ) {
+            let heightIn = 12 * Number(values.height) + Number(values.heightin);
+            let weightLbs = (Number(values.weight) * 14) + Number(values.weightlbs);
+            setResultBmi((weightLbs / (heightIn * heightIn) * 703).toFixed(1));
+        } else {
+            setResultBmi("-");
+        }
+    }
+
+    function calcIdealWeight(height){
+        let startRange = 18.5 * (height * height);
+        let endRange = 24.9 * (height * height);
+        setIdealWeight({
+            startrange: startRange.toFixed(1),        endrange: endRange.toFixed(1)
+        })
     }
 
     return(
         <div className="calculator">
             <h4 className="calculator__title">Enter your details below</h4>
             <RadioSelectUnit unitSelected={getUnit}/>
-            <Form show={unitShowed}></Form>
+            <Form show={unitShowed} valuesInput={getValues}></Form>
             <div className="result">
                 <div className="result__score">
                     <h5 className="result__title">Your BMI is...</h5>
-                    <span className="result__bmi">23.4</span>
+                    <span className="result__bmi">{resultBmi}</span>
                 </div>
                 <div className="result__comment-wrapper">
-                    <p className="result__comment">Your BMI suggests you're a healthy weight Your ideal weight is between <strong>63.3kgs-85.2kgs.</strong></p>
+                    <p className="result__comment">Your BMI suggests you're a {classficationBmi} Your ideal weight is between <strong>{idealWeight.startrange}-{idealWeight.endrange}.</strong></p>
                 </div>
             </div>
         </div>
-
     );
 }
--- junior/bmi-calculator/src/components/Form.jsx
+++ junior/bmi-calculator/src/components/Form.jsx
@@ -1,49 +1,67 @@
 import React,{ useState } from "react";
 import "../components/Form.css";
 
-export default function Form({show}){
+export default function Form({show, valuesInput}){
 
-    const [focus, setFocus] = useState(false);
-    const [heightMetric, setHeightMetric] = useState("");
-    const [weightMetric, setWeightMetric] = useState("");
-    const [heightImperialFt, setHeightImperialFt] = useState("");
-    const [heightImperialIn, setHeightImperialIn] = useState("");
-    const [weightImperialSt, setWeightImperialSt] = useState("");
-    const [weightImperialLbs, setWeightImperialLbs] = useState("");
+    const [formMetric, setFormMetric] = useState({
+        height: "",
+        weight: "",
+    });
 
+    const [formImperial, setFormImperial] = useState({
+        height: "",
+        weight: "",
+        heightin: "",
+        weightlbs: "",
+    });
+ 
     function handleFocus(e){
         const formInputWrapper = e.target.parentElement;
-        if (focus === false) {
-            setFocus(true);
-            formInputWrapper.classList.add("form__input-wrapper--focus");
+        formInputWrapper.classList.add("form__input-wrapper--focus");
+    }
+
+    function handleBlur(e){
+        const formInputWrapper = e.target.parentElement;
+        formInputWrapper.classList.remove("form__input-wrapper--focus");
+        if (show === "metric") {
+            valuesInput(formMetric);
         } else {
-            setFocus(false);
-            formInputWrapper.classList.remove("form__input-wrapper--focus");
+            valuesInput(formImperial);
+        }
+    }
+
+    function handleChange(e){
+        
+        if (show === "metric") {
+            setFormMetric({
+                ...formMetric,
+                [e.target.name]: e.target.value
+            });
+        } else {
+            setFormImperial({
+                ...formImperial,
+                [e.target.name]: e.target.value
+            });
         }
     }
 
     return(
-        <form className="form">
+        <form className="form" >
             {show === "metric" ? 
             (<>
                  {/* metric */}
                 <div className="form__field-metric-wrapper">
                     <label className="form__field" htmlFor="height">
                         Height
-                        <div className="form__input-wrapper" tabIndex={0}>
-                            <input
-                                className="form__input" type="number" id="height" name="height"  placeholder="0" min={0} onFocus={handleFocus} onBlur={handleFocus}
-                                value={heightMetric}
-                                onChange={e => setHeightMetric(e.target.value)}/>
+                        <div className="form__input-wrapper" >
+                            <input className="form__input" type="number" id="height" name="height" placeholder="0" tabIndex={0} min={0} max={251} onFocus={handleFocus} onBlur={handleBlur} value={formMetric.height} onChange={handleChange}/>
                             <span className="form__unit">cm</span>
                         </div>
                     </label>
                     <label className="form__field" htmlFor="weight">
                         Weight
-                        <div className="form__input-wrapper" tabIndex={0}>
-                            <input
-                                className="form__input" type="number" id="weight" name="weight"  placeholder="0" min={0} onFocus={handleFocus} onBlur={handleFocus} value={weightMetric}
-                                onChange={e => setWeightMetric(e.target.value)}/>
+                        <div className="form__input-wrapper">
+                            <input className="form__input" type="number" id="weight" name="weight"  placeholder="0" tabIndex={0} min={0} max={600} onFocus={handleFocus} onBlur={handleBlur} value={formMetric.weight} onChange={handleChange}/>
                             <span className="form__unit">kg</span>
                         </div>
                     </label>
@@ -52,50 +70,35 @@ export default function Form({show}){
             (<>
                 {/* imperial */}
                 <div className="form__field-imperial-wrapper">
-                    <label className="form__field" htmlFor="heightft">
+                <label className="form__field" htmlFor="height">
                         Height
-                        <div className="form__input-wrapper" tabIndex={0}>
-                            <input
-                                className="form__input" type="number" id="heightft" name="heightft"  placeholder="0" min={0} onFocus={handleFocus} onBlur={handleFocus}
-                                value={heightImperialFt}
-                                onChange={e => setHeightImperialFt(e.target.value)}/>
+                        <div className="form__input-wrapper" >
+                            <input className="form__input" type="number" id="height" name="height" placeholder="0" tabIndex={0} min={0} max={10} onFocus={handleFocus} onBlur={handleBlur} value={formImperial.height} onChange={handleChange}/>
                             <span className="form__unit">ft</span>
                         </div>
                     </label>
                     <label className="form__field" htmlFor="heightin">
-                        <div className="form__input-wrapper" tabIndex={0}>
-                            <input
-                                className="form__input" type="number" id="heightin" name="heightin"  placeholder="0" min={0} onFocus={handleFocus} onBlur={handleFocus}
-                                value={heightImperialIn}
-                                onChange={e => setHeightImperialIn(e.target.value)}/>
+                        <div className="form__input-wrapper">
+                            <input className="form__input" type="number" id="heightin" name="heightin"  placeholder="0" tabIndex={0} min={0} onFocus={handleFocus} onBlur={handleBlur} value={formImperial.heightin} onChange={handleChange}/>
                             <span className="form__unit">in</span>
                         </div>
                     </label>
                 </div>
                 <div className="form__field-imperial-wrapper">
-                    <label className="form__field" htmlFor="weightst">
+                    <label className="form__field" htmlFor="weight">
                         Weight
-                        <div className="form__input-wrapper" tabIndex={0}>
-                            <input 
-                                className="form__input" type="number" id="weightst" name="weightst"  placeholder="0" min={0} onFocus={handleFocus} onBlur={handleFocus} 
-                                value={weightImperialSt}
-                                onChange={e => setWeightImperialSt(e.target.value)}/>
-
+                        <div className="form__input-wrapper" >
+                            <input className="form__input" type="number" id="weight" name="weight" placeholder="0" tabIndex={0} min={0} max={50} onFocus={handleFocus} onBlur={handleBlur} value={formImperial.weight} onChange={handleChange}/>
                             <span className="form__unit">st</span>
-                        </div> 
+                        </div>
                     </label>
                     <label className="form__field" htmlFor="weightlbs">
-                        <div className="form__input-wrapper" tabIndex={0}>
-                            <input 
-                                className="form__input" type="number" id="weightlbs" name="weightlbs"  placeholder="0" min={0} onFocus={handleFocus} onBlur={handleFocus} 
-                                value={weightImperialLbs}
-                                onChange={e => setWeightImperialLbs(e.target.value)}/>
-
+                        <div className="form__input-wrapper">
+                            <input className="form__input" type="number" id="weightlbs" name="weightlbs"  placeholder="0" tabIndex={0} min={0} onFocus={handleFocus} onBlur={handleBlur} value={formImperial.weightlbs} onChange={handleChange}/>
                             <span className="form__unit">lbs</span>
-                        </div> 
+                        </div>
                     </label>
-                </div>
-                
+                </div> 
             </>)}
         </form>
     );
