09e34c4a43b4ca8c0b3c01f8937dfa0419d6d7cf
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-07-31 21:53:00 -0300

    responsividade desktop ajustada

--- junior/bmi-calculator/src/App.jsx
+++ junior/bmi-calculator/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import logo from "./images/logo.svg";
 import man from "./images/image-man-eating.webp";
 import Calculator from "./components/Calculator";
+import Advices from "./components/Advices";
 import AttributionFooter from "./components/AttributionFooter";
 
 export default function App(){
@@ -33,6 +34,7 @@ export default function App(){
                         <p className="result-explanation__text">A BMI range of 18.5 to 24.9 is considered a 'healthy weight.' Maintaining a healthy weight may lower your chances of experiencing health issues later on, such as obesity and type 2 diabetes. Aim for a nutritious diet with reduced fat and sugar content, incorporating ample fruits and vegetables. Additionally, strive for regular physical activity, ideally about 30 minutes daily for five days a week.</p>
                     </div>
                 </section>
+                <Advices></Advices>
             </main>
             <footer className="footer">
                 <AttributionFooter></AttributionFooter>
