ee446b57ef35694e8a769247628ff72be71a3a34
Rafael Dias <94787506+rafaeldgeo@users.noreply.github.com>
2024-08-01 22:24:39 -0300

    desenvolvendo component tip desktop

---
+++ junior/bmi-calculator/src/components/Tips.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../components/Tips.css";
+import { tips } from "./data.js";
+
+export default function Tips(){
+
+    const listTips = tips.map(tip => 
+        <div key={tip.id} className="tips__card">
+            <div className="tips__icon-wrapper">
+                <img className="tips__icon" src={tip.icon} alt="" width={64} height={64}/></div>
+            <div className="tips__content">
+                <h3 className="tips__title">{tip.title}</h3>
+                <p className="tips__text">{tip.text}</p>
+            </div>
+        </div>
+    );
+
+    return(
+        <div className="tips">{listTips}</div>
+    );
+}
+
+
---
+++ junior/bmi-calculator/src/components/data.js
@@ -0,0 +1,24 @@
+import iconEating from "../images/icon-eating.svg";
+import iconExercise from "../images/icon-exercise.svg";
+import iconSleepp from "../images/icon-sleep.svg";
+
+export const tips = [
+    {
+        id: 0,
+        icon: iconEating,
+        title: "Healthy eating",
+        text: "Healthy eating promotes weight control, disease prevention, better digestion,  immunity, mental clarity, and mood."
+    },
+    {
+        id: 1,
+        icon: iconExercise,
+        title: "Regular exercise",
+        text: "Exercise improves fitness, aids weight control, elevates mood, and reduces disease risk, fostering wellness and longevity."
+    },
+    {
+        id: 3,
+        icon: iconSleepp,
+        title: "Adequate sleep",
+        text: "Sleep enhances mental clarity, emotional stability, and physical wellness, promoting overall restoration and rejuvenation."
+    }
+];
