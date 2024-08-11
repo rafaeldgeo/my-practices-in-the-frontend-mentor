4d3bd32c912b33e5b437e6911f2bdfd69ac42b98
rafaeldgeo <rrfldias@gmail.com>
2024-08-11 18:11:55 -0300

    responsividade do result para tablet finalizado

--- junior/bmi-calculator/src/App.jsx
+++ junior/bmi-calculator/src/App.jsx
@@ -20,7 +20,7 @@ export default function App(){
                             <h1 className="hero__title">Body Mass Index Calculator</h1>
                             <p className="hero__introduction">Better understand your weight in relation to your height using our body mass index (BM) calculator. While BMI is not the sole determinant of a healthy weight, it offers a valuable starting point to evaluate your overall health and well-being.</p>
                         </div>
-                        <Calculator />
+                        <Calculator /> 
                     </div>
                 </div> 
                 <div className="hero__background"></div> 
