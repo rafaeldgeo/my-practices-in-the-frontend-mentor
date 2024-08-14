789e816ab05861dde9c2b7b5cf77fc3966e0fd56
rafaeldgeo <rfldiasapp@gmail.com>
2024-08-13 21:40:17 -0300

    desenvolvendo tip

--- junior/bmi-calculator/src/components/Tips.jsx
+++ junior/bmi-calculator/src/components/Tips.jsx
@@ -7,7 +7,8 @@ export default function Tips(){
     const listTips = tips.map(tip => 
         <div key={tip.id} className="tips__card">
             <div className="tips__icon-wrapper">
-                <img className="tips__icon" src={tip.icon} alt="" width={64} height={64} loading="lazy"/></div>
+                <img className="tips__icon" src={tip.icon} alt="" width={64} height={64} loading="lazy"/>
+            </div>
             <div className="tips__content">
                 <h3 className="tips__title">{tip.title}</h3>
                 <p className="tips__text">{tip.text}</p>
