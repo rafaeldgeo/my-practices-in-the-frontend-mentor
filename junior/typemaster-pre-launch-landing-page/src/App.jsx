1e26561db493ff59b313fca2d943bd49e5642388
rafaeldgeo <rfldiasapp@gmail.com>
2024-08-29 21:42:17 -0300

    desenvolvendo esqueleto inicial

---
+++ junior/typemaster-pre-launch-landing-page/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import AttributionFooter from "./components/AttributionFooter";
+
+export default function App(){
+
+    return(
+        <div className="container">
+            <header className="header">
+
+            </header>
+            <main className="main">
+
+            </main>
+            <footer className="footer">
+                <p>Typemaster 2021 | All Rights Reserved</p>
+                <AttributionFooter />
+            </footer>
+        </div>
+    );
+}
