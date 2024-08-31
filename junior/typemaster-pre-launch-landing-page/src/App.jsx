0ded9761dbe61bc3ca06204f1c517f28af9195f6
rafaeldgeo <rfldiasapp@gmail.com>
2024-08-31 17:58:35 -0300

    header finalizado

--- junior/typemaster-pre-launch-landing-page/src/App.jsx
+++ junior/typemaster-pre-launch-landing-page/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import logo from "./assets/shared/logo.svg";
 import AttributionFooter from "./components/AttributionFooter";
 
 export default function App(){
@@ -7,7 +8,10 @@ export default function App(){
     return(
         <div className="container">
             <header className="header">
-
+                <img className="header__logo" src={logo} alt="logo TM" width={40} height={40}/>
+                <div className="header__btn-wrapper">
+                    <button className="btn__pre-order" type="button">Pre-order now</button>
+                </div>
             </header>
             <main className="main">
 
