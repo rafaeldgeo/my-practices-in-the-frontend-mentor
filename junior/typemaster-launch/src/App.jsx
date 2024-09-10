f2b0abb39f79cf5b183065d09cf0e3dcdc631158
rafaeldgeo <rfldiasapp@gmail.com>
2024-09-09 22:05:56 -0300

    desenvolvendo mobile

--- junior/typemaster-launch/src/App.jsx
+++ junior/typemaster-launch/src/App.jsx
@@ -1,11 +1,30 @@
 import React from "react";
+import logo from "./assets/shared/logo.svg";
 import "./App.css";
 
 export default function App(){
 
     return(
-        <>
-            texto
-        </>
+        <div className="container">
+            <header className="header">
+                <img className="header__logo" src={logo} alt="orange square containing the letters tm" width={40} height={40} />
+                <button className="header__btn btn-order"
+                 type="button">Pre-order now</button>
+            </header>
+            <main className="main">
+                <section className="highline">
+                    <div className="highline__content">
+                        <h1 className="highline__title">Typemaster keyboard</h1>
+                        <p className="highline__text">Improve your productivity and gaming without breaking the bank. Upgrade to a high quality 
+                        mechanical typing experience.</p>
+                        <div className="highline__order">
+                            <button className="highline__btn btn-order" type="button">Pre-order now</button>
+                            <span className="highline__release">Release on 5/27</span>
+                        </div>
+                    </div>
+                </section>
+            </main>
+            <footer className="footer"></footer>
+        </div>
     );
 }
