94d20e9ecbe3ac0b2c22535f6016d307db73951e
rafaeldgeo <rfldiasapp@gmail.com>
2024-09-10 21:33:45 -0300

    desenvolvendo mobile

--- junior/typemaster-launch/src/App.jsx
+++ junior/typemaster-launch/src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import logo from "./assets/shared/logo.svg";
+import mobileKeyboard from "./assets/mobile/image-keyboard.jpg";
+import tabletKeyboard from "./assets/tablet/image-keyboard.jpg";
+import desktopKeyboard from "./assets/desktop/image-keyboard.jpg";
 import "./App.css";
 
 export default function App(){
@@ -17,11 +20,17 @@ export default function App(){
                         <h1 className="highline__title">Typemaster keyboard</h1>
                         <p className="highline__text">Improve your productivity and gaming without breaking the bank. Upgrade to a high quality 
                         mechanical typing experience.</p>
-                        <div className="highline__order">
+                        <div className="highline__order-wrapper">
                             <button className="highline__btn btn-order" type="button">Pre-order now</button>
                             <span className="highline__release">Release on 5/27</span>
                         </div>
                     </div>
+                    <picture className="highline__image-wrapper">
+                        <source srcSet={mobileKeyboard} media="(max-width: 375px)" width={375} height={331}/>
+                        <source srcset="https://placehold.co/956x850" media="(min-width: 376px) and (max-width: 768px)"/>
+                        <source srcset="https://placehold.co/1080x960" media="(min-width: 769px)"/>
+                        <img className="highline__image" src={mobileKeyboard} alt="black keyboard"/>
+                    </picture>
                 </section>
             </main>
             <footer className="footer"></footer>
