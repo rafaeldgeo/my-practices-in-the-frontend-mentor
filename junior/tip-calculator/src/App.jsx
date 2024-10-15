6317bf1a3c7d7f8f0438fa4f7ec0934695f027ed
rafael <rfldiasapp@gmail.com>
2024-10-14 21:54:58 -0300

    desenvolvendo

--- junior/tip-calculator/src/App.jsx
+++ junior/tip-calculator/src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./App.css";
+import AttributionFooter from "./components/AttributionFooter";
 
 export default function App() {
 
     return(
-        <>
-            teste
-        </>
+        <div className="container">
+            <header className="header"></header>
+            <main className="main"></main>
+            <footer className="footer">
+                <AttributionFooter />
+            </footer>
+        </div>
     );
 }
