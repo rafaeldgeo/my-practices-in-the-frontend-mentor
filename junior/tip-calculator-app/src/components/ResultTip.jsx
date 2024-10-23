79e7a4e3afef0a3795e83dcbd3d7f1c21f9b579d
rafael <rfldiasapp@gmail.com>
2024-10-22 21:30:12 -0300

    layout desktop finalizado

--- junior/tip-calculator-app/src/components/ResultTip.jsx
+++ junior/tip-calculator-app/src/components/ResultTip.jsx
@@ -8,16 +8,13 @@ export default function ResultTip(){
         <div className="result-tip">
             <div className="result">
                 <div className="result__tip">
-                    <div>
-                        <h2 className="result__label">Tip Amount</h2>
-                        <span className="result__per-person">/ person</span>
-                    </div>
-                    <div><span className="result__value">$ 4.27</span></div>
+                    <h2 className="result__label">Tip Amount<span className="result__by-person" aria-label="division by person">/ person</span></h2>
+                    <span className="result__value" aria-label="value">0.00</span>
                 </div>
-                {/* <div className="result__tip">
-                    <h2 className="result__label">Tip Amount<span className="result__per-person">/ person</span></h2>
-                    <span className="result__value">$ 32.79</span>
-                </div> */}
+                <div className="result__tip">
+                    <h2 className="result__label">Total<span className="result__by-person" aria-label="division by person">/ person</span></h2>
+                    <span className="result__value" aria-label="value">0.00</span>
+                </div>
             </div>
             <div className="btn">
                 <button className="btn__reset" type="button">Reset</button>
