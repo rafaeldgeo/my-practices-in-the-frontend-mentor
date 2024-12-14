c29582053c65ccab946fd9a485072844fabd3266
rafael <rfldiasapp@gmail.com>
2024-12-13 21:00:03 -0300

    sr-only inserido

--- junior/tip-calculator-app/src/App.jsx
+++ junior/tip-calculator-app/src/App.jsx
@@ -9,6 +9,7 @@ export default function App() {
     return(
         <div className="container">
             <main className="main">
+                <h1 class="sr-only">Tip Calculator</h1>
                 <div className="logo">
                     <img className="logo__img" src={logo} alt="logo" width={87} height={54} />
                 </div>
