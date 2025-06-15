51a17bd304d832d9bdcb571be2eeb3a779f6375b
Rafael <rfldiasapp@gmail.com>
2025-06-15 20:22:08 -0300

    focus nos elementos finalizado

--- junior/mortgage-repayment-calculator-main/public/js/form.js
+++ junior/mortgage-repayment-calculator-main/public/js/form.js
@@ -1,37 +1,47 @@
-const formInput = document.querySelectorAll(".form__input-container");
-const form = document.querySelector(".form");
+const formInputs = document.querySelectorAll(".form__input-container");
+const formRadios = document.querySelectorAll(".form__radio-container");
 
-// const focusInput = function (e) {
-//     const formInputContainer = e.target.parentElement;
-//     const labelSecondary = formInputContainer.querySelector(".form__label-secondary");
-//     const backgroundColor = getComputedStyle(labelSecondary)["background-color"];
-//     if (backgroundColor === "rgb(227, 243, 253)") {
-//         formInputContainer.style.borderColor = "hsl(61, 70%, 52%)";
-//         labelSecondary.style.backgroundColor = "hsl(61, 70%, 52%)";
-//         labelSecondary.style.color = "hsl(202, 55%, 16%)";
-//     } else {
-//         formInputContainer.style.borderColor = "hsl(200, 24%, 40%)";
-//         labelSecondary.style.backgroundColor = "hsl(202, 86%, 94%)";
-//         labelSecondary.style.color = "hsl(200, 24%, 40%)";
-//     }
-// }
+const focusInput = function (e) {
+    const clickedFormInputContainer = e.target.parentElement;
+    const labelSecondary = clickedFormInputContainer.querySelector(".form__label-secondary");
+    if (!labelSecondary) return;
 
-const focusInput = function(e) {
-    const formInputContainer = e.target.parentElement;
-    const labelSecondary = formInputContainer.querySelector(".form__label-secondary");
-    const isFocus = formInputContainer.getAttribute
+    if (e.type === "focusin") {
+        clickedFormInputContainer.classList.add("form__input-container--focus");
+        clickedFormInputContainer.classList.remove("form__input-container--hover");
+        labelSecondary.classList.add("form__label-secondary--focus");
+    }
+
+    if (e.type === "focusout") {
+        clickedFormInputContainer.classList.remove("form__input-container--focus");
+        clickedFormInputContainer.classList.add("form__input-container--hover");
+        labelSecondary.classList.remove("form__label-secondary--focus");
+    }
 }
 
-form.addEventListener("focusin", focusInput);
-form.addEventListener("focusout", focusInput);
+formInputs.forEach((input) => {
+    input.addEventListener("focusin", focusInput);
+    input.addEventListener("focusout", focusInput);
+});
 
+const clickRadio = function (e) {
+    const formRadioContainers = document.querySelectorAll(".form__radio-container");
+    formRadioContainers.forEach((radio) => {
+        if (radio.classList.contains("form__radio--checked")) {
+            radio.classList.remove("form__radio--checked");
+        }
+    });
+    const clickedFormRadioContainer = e.target.closest(".form__radio-container");
+    if (!clickedFormRadioContainer) return;
+    const isChecked = clickedFormRadioContainer.querySelector(".form__radio").checked;
+    if (isChecked) {
+        clickedFormRadioContainer.classList.add("form__radio--checked");
+    }
+}
 
+formRadios.forEach((radio) => {
+    radio.addEventListener("click", clickRadio);
+});
 
-
-// formInput.forEach((input) => {
-//     input.addEventListener("focusin", (event) => {
-//         console.log(event.target.parentElement);
-//     })
-// })
 
 
