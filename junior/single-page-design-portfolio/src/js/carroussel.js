467a5d1bb228a456e902ffd8e6048189483ebd5e
Rafael <rfldiasapp@gmail.com>
2025-10-31 22:18:44 -0300

    continua desenvolvimento carrousel

--- junior/single-page-design-portfolio/src/js/carroussel.js
+++ junior/single-page-design-portfolio/src/js/carroussel.js
@@ -3,7 +3,7 @@
 const carouselListUl = document.querySelector(".carousel__list");
 const controlNextBtn = document.querySelector(".carousel__control-btn--next");
 const controlPreviousBtn = document.querySelector(".carousel__control-btn--previous");
-const firstItemLi = carouselListUl.firstChild.cloneNode(true);
+const firstItemLi = carouselListUl.children;
 const lastItemLi = carouselListUl.lastChild.cloneNode(true);
 
 if (!carouselListUl || !controlNextBtn || !controlPreviousBtn || !firstItemLi || !lastItemLi) {
@@ -14,8 +14,8 @@ if (carouselListUl.childElementCount === 0) {
     throw new Error("Imagens not found");
 }
 
-carouselListUl.appendChild(firstItemLi);
-carouselListUl.insertBefore(lastItemLi, carouselListUl.firstElementChild);
+// carouselListUl.appendChild(firstItemLi);
+// carouselListUl.insertBefore(lastItemLi, carouselListUl.firstElementChild);
 
 
 
