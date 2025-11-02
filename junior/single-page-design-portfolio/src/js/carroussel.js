a153ac2882d0be5b114abb4910cd9ea699f9485a
Rafael <rfldiasapp@gmail.com>
2025-11-02 17:11:39 -0300

    carrosel simples em desenvolvimento

--- junior/single-page-design-portfolio/src/js/carroussel.js
+++ junior/single-page-design-portfolio/src/js/carroussel.js
@@ -1,89 +1,47 @@
 "use strict";
 
-const carouselListUl = document.querySelector(".carousel__list");
-const slidesLi = carouselListUl.querySelectorAll(".carousel__item");
+const carouselList = document.querySelector(".carousel__list");
+let slides = carouselList.querySelectorAll(".carousel__item");
 const nextBtn = document.querySelector(".carousel__control-btn--next");
 const previousBtn = document.querySelector(".carousel__control-btn--previous");
-const firstItemLi = carouselListUl.firstElementChild.cloneNode(true);
-const lastItemLi = carouselListUl.lastElementChild.cloneNode(true);
 
-if (!carouselListUl || !nextBtn || !previousBtn || !firstItemLi || !lastItemLi) {
+if (!carouselList || !nextBtn || !previousBtn) {
     throw new Error("Carousel, button next, button previous, first image or last image not found");
 }
 
-if (carouselListUl.childElementCount === 0 || slidesLi.length === 0) {
+if (carouselList.childElementCount === 0) {
     throw new Error("Imagens not found");
 }
 
-// Append the last image in begin and first image in end of the list. This made the transition of carousel infinite
-lastItemLi.classList.add("carousel__item--last-clone");
-carouselListUl?.insertBefore(lastItemLi, carouselListUl.firstElementChild);
-firstItemLi.classList.add("carousel__item--first-clone");
-carouselListUl?.appendChild(firstItemLi);
-
-
-// CAROUSEL RESPONSIVE AND INFINIT TRANSITION
-let currentIndex = 3;
+let currentIndex = 0;
 updateCarouselPosition();
 
-// Get width imagem(slide) and column-gap. It's necessary when it will define the translateX, because when view reduce or increase the translateX changes
 function getSlideWidth() {
-    const slide = slidesLi[0];
-    const slideWidth = slide.offsetWidth;
-    const gap = parseFloat(getComputedStyle(carouselListUl).columnGap) || 0;
-    return slideWidth + gap;
+    const slide = slides[0];
+    const gap = parseFloat(getComputedStyle(carouselList).columnGap) || 0;
+    return slide.offsetWidth + gap;
 }
 
-// Update de position of slide
 function updateCarouselPosition() {
     const offset = - getSlideWidth() * currentIndex;
-    carouselListUl.style.transform = `translateX(${offset}px)`;
+    carouselList.style.transform = `translateX(${offset}px)`;
 }
 
-// control of the navigation for carousel
 nextBtn.addEventListener("click", () => {
-    currentIndex++;
-    if (currentIndex >= slidesLi.length) {
-        currentIndex = 0;
-        handleLoop();
+    if (currentIndex < slides.length - 3) {
+        carouselList.style.transition = "transform 0.5s ease";
+        currentIndex++;
+    }
+    updateCarouselPosition()
+});
+
+previousBtn.addEventListener("click", () => {
+    carouselList.style.transition = "transform 0.5s ease";
+    if (currentIndex > 0) {
+        currentIndex--;
     }
     updateCarouselPosition();
 });
 
-previousBtn.addEventListener("click", () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-        handleLoop();
-        updateCarouselPosition();
-    }
-    
-});
-
 // recalc the size view of the slide
 window.addEventListener("resize", updateCarouselPosition);
-
-function handleLoop() {
-    carouselListUl.addEventListener("transitionend", () => {
-        if (slidesLi[currentIndex].classList.contains("carousel__item--first-clone")) {
-            carouselListUl.style.transition = "none"; // off transition
-            currentIndex = 1;
-            carouselListUl.style.transform = `translateX(${-getSlideWidth() * currentIndex}px)`;
-        }
-
-        if (slidesLi[currentIndex].classList.contains("last-clone")) {
-            carouselListUl.style.transition = "none";
-            currentIndex = slidesLi.length - 2;
-            carouselListUl.style.transform = `translateX(${-getSlideWidth() * currentIndex}px)`;
-        }
-    }, {once: true}); // shoot once
-}
-
-
-
-
-
-
-
-
-
-
