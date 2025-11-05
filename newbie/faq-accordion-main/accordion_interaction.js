c40c07b32f266a9936c04988ba24e665063e4b23
Rafael <rfldiasapp@gmail.com>
2025-11-05 20:38:51 -0300

    aperfeiçoamento de javascript

--- newbie/faq-accordion-main/accordion_interaction.js
+++ newbie/faq-accordion-main/accordion_interaction.js
@@ -2,15 +2,20 @@
 
 const accordionSummaries = document.querySelectorAll(".faq__accordion-summary");
 
+if (accordionSummaries.length === 0) {
+  throw new Error ("Don't found element summary. Accordion cannot initialize.");
+}
+
+// listen to tag summary
 accordionSummaries.forEach((summary) => {
   summary.addEventListener("click", closeOpenedDetails);
 });
 
+// function close all details allow only one opened
 function closeOpenedDetails() {
-
   accordionSummaries.forEach((summary) => {
-    let detail = summary.parentNode;
-    if (detail != this.parentNode) {
+    let detail = summary.parentNode; // catch the element father, tag details
+    if (detail !== this.parentNode) { // this.parentNode is the detail clicked
       detail.removeAttribute("open");
     }
   });
