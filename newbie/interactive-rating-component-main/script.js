ccd2f5e032808de6d53d1208a2544d1b656a4678
Rafael <rfldiasapp@gmail.com>
2025-11-06 20:35:26 -0300

    ajustes javascript

--- newbie/interactive-rating-component-main/script.js
+++ newbie/interactive-rating-component-main/script.js
@@ -1,28 +1,36 @@
 "use strict";
 
-document.querySelector(".card-rate__submit-btn").addEventListener("click", showThanks);
-document.querySelector(".card-rate__grades").addEventListener("click", selectGrade);
+const btnGrades = document.querySelectorAll(".card-rate__note-input");
+const ratingSection = document.querySelector(".card-rate");
+const btnSubmit = document.querySelector(".card-rate__submit-btn");
+const spanGrade = document.querySelector(".card-thanks__note");
+const thanksSection = document.querySelector(".card-thanks");
 
+if (btnGrades.length === 0 || !ratingSection || !btnSubmit || !spanGrade || !thanksSection) {
+  throw new Error("Some component not found!");
+}
+
+// select the button and capture the grade 
+function selectGrade(e) {
+  const btnSelected = e.target;
+  const grade = btnSelected.value;
+
+  for (let btn of btnGrades) {
+    btn.classList.remove("card-rate__note-input--selected");
+  }
+
+  btnSelected.classList.add("card-rate__note-input--selected");
+  spanGrade.textContent = grade;
+}
+
+// hide rating and show success message
 function showThanks() {
-  const ratingSection = document.querySelector(".card-rate");
-  const thanksSection = document.querySelector(".card-thanks");
   ratingSection.style.display = "none";
   thanksSection.style.display = "block";
 }
 
-function selectGrade(){
-  const listInput = document.querySelectorAll(".card-rate__note-input");
-  const spanGrade = document.querySelector(".card-thanks__note");
-  const inputSelected = event.target;
-  const grade = inputSelected.value;
-  
-  for (let input of listInput) {
-    input.classList.remove("card-rate__note-input--selected");  
-  }
-  
-  if (inputSelected.tagName == "INPUT") {
-    inputSelected.classList.add("card-rate__note-input--selected");
-    spanGrade.innerText = grade;
-  }
+btnGrades.forEach(btn => {
+  btn.addEventListener("click", selectGrade);
+});
 
-}
+btnSubmit.addEventListener("click", showThanks);
