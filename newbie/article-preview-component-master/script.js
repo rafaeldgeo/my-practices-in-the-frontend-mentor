e81df4c66a2d0e252f7a16c6706a4f50d1aafdfe
Rafael <rfldiasapp@gmail.com>
2025-11-06 20:52:05 -0300

    correção javascript

--- newbie/article-preview-component-master/script.js
+++ newbie/article-preview-component-master/script.js
@@ -2,20 +2,22 @@
 
 const articleDivShare = document.querySelector(".article__div-share");
 const articleBtn = document.querySelector(".article__btn");
+const articleDivUser = document.querySelector(".article__div-user");
+const articleDivMidias = document.querySelector(".article__div-midias");
+const articleDivFooter = document.querySelector(".article__div-footer");
+const articleSvgArrow = document.querySelector(".article__svg-arrow");
 
+if (!articleDivShare || !articleBtn || !articleDivUser || !articleDivMidias || !articleDivFooter || !articleSvgArrow) {
+  throw new Error ("Some component not found");
+}
+
+// change the style of footer for the card
 const share = function() {
-  const articleDivUser = document.querySelector(".article__div-user");
-  const articleDivMidias = document.querySelector(".article__div-midias");
-  const articleDivFooter = document.querySelector(".article__div-footer");
-  const articleBtn = document.querySelector(".article__btn");
-  const articleSvgArrow = document.querySelector(".article__svg-arrow");
-  
   articleDivUser.classList.toggle("article__div-user--changeview");
   articleDivMidias.classList.toggle("article__div-midias--show");
   articleDivFooter.classList.toggle("article__div-footer--changecolor");
   articleBtn.classList.toggle("article__btn--dark");
   articleSvgArrow.classList.toggle("article__svg-arrow--white");
-
 }
 
 articleDivShare.addEventListener("click", share);
