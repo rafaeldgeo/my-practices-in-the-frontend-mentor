11c7bace1ab475a14bdbc15f6fe4277cdbdd8d89
Rafael <rfldiasapp@gmail.com>
2025-11-05 21:25:24 -0300

    ajuste codigo javascript

--- newbie/ping-coming-soon-page-master/validform.js
+++ newbie/ping-coming-soon-page-master/validform.js
@@ -4,21 +4,27 @@ const mainForm = document.querySelector(".main__form");
 const mainInput = document.querySelector(".main__input");
 const mainError = document.querySelector(".main__error");
 
+if (!mainForm || !mainInput || !mainError) {
+  console.warn("elements form, input or spanError not found");
+}
+
+// listen the input and check if it was fill and email valid
 mainInput.addEventListener("input", 
   function() {
-    if (mainInput.validity.valid && mainInput.value.length > 0) {
+    if (mainInput.validity.valid && mainInput.value.length > 0) { //validity property check the real state of the input (Standard HTML)
       mainError.innerText = "";
-      mainInput.classList.remove("active");
+      mainInput.classList.remove("active"); 
     }
   }
 );
 
+// listen the submit and check if it was fill and email valid
 mainForm.addEventListener("submit", 
   function(e) {
-    if (!mainInput.validity.valid || mainInput.value.length === 0) {
+    if (!mainInput.validity.valid || mainInput.value.length === 0) { 
       mainError.innerText = "Please provide a valid email address";
-      mainInput.classList.add("active");
-      e.preventDefault();
+      mainInput.classList.add("active"); 
+      e.preventDefault(); // don't allow to sent the form
     } else {
       mainError.innerText = "";
     }
