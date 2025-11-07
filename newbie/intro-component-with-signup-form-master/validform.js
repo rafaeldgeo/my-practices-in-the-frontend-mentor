86fd6590767d2689374b76442196798a4afd283c
Rafael <rfldiasapp@gmail.com>
2025-11-07 19:52:43 -0300

    ajustes de javascript e exclusão pasta design

--- newbie/intro-component-with-signup-form-master/validform.js
+++ newbie/intro-component-with-signup-form-master/validform.js
@@ -1,27 +1,43 @@
 "use strict";
 
 const clainForm = document.querySelector(".clain__form");
-const clainInput = document.querySelectorAll(".clain__input");
+const clainInputs = document.querySelectorAll(".clain__input");
+let hasError = false;
 
-clainForm.addEventListener("input", 
-  function() {
-    for (let i = 0; i < clainInput.length; i++) {
-      if (clainInput[i].validity.valid || clainInput[i].value.length > 0) {
-        clainInput[i].removeAttribute("required");
-        clainInput[i].nextElementSibling.style.display = "none";
+if (!clainForm) {
+  throw new Error("Form not found");
+}
+
+if (clainInputs.length === 0) {
+  throw new Error("Inputs not found");
+}
+
+// check if all inputs are valid
+clainForm.addEventListener("input",
+  function () {
+    for (let i = 0; i < clainInputs.length; i++) {
+      if (clainInputs[i].validity.valid || clainInputs[i].value.length > 0) {
+        hasError = false;
+        clainInputs[i].removeAttribute("required");
+        clainInputs[i].nextElementSibling.style.display = "none";
       }
-    }    
+    }
   }
 );
 
+// check if all inputs are valid and set erro
 clainForm.addEventListener("submit",
-   function(e) {
-     for (let i = 0; i < clainInput.length; i++) {
-       if (!clainInput[i].validity.valid || clainInput[i].value.length == 0) {
-         clainInput[i].setAttribute("required", "required");
-         clainInput[i].nextElementSibling.style.display = "block";
-       }
-     }
-     e.preventDefault(); 
-   }      
+  function (e) {
+    for (let i = 0; i < clainInputs.length; i++) {
+      if (!clainInputs[i].validity.valid || clainInputs[i].value.length == 0) {
+        hasError = true;
+        clainInputs[i].setAttribute("required", "required");
+        clainInputs[i].nextElementSibling.style.display = "block";
+      }
+    }
+    if (hasError) {
+      e.preventDefault();
+    }
+  }
 );
+
