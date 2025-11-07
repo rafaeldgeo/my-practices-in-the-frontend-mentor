e7bd01ddc1001b5218339090cd4f53a7422a8b5e
Rafael <rfldiasapp@gmail.com>
2025-11-07 20:22:03 -0300

    ajustes do javascript e exclusão pasta node_modules

--- newbie/pod-request-access-landing-page/src/validemail.js
+++ newbie/pod-request-access-landing-page/src/validemail.js
@@ -4,8 +4,17 @@ const form = document.querySelector(".form");
 const formInput = document.querySelector(".form__input");
 const formWrapperErro = document.querySelector(".form-wrapper__erro");
 
+if (!form || !formInput) {
+    throw new Error("Form or input not found");
+}
+
+if (!formWrapperErro) {
+    console.warn("Message erro not found");
+}
+
+// check if the input is valid
 formInput.addEventListener("input",
-    function() {
+    function () {
         if (formInput.validity.valid && formInput.value.length > 0) {
             form.classList.remove("form--erro-show");
             formInput.classList.remove("form__input__erro--show");
@@ -14,9 +23,10 @@ formInput.addEventListener("input",
     }
 );
 
-form.addEventListener("submit", 
-    function(e) {
-        if (!formInput.validity.valid || form.value.length === 0) {
+// check if the input was filled and sets error
+form.addEventListener("submit",
+    function (e) {
+        if (!formInput.validity.valid || formInput.value.length === 0) {
             form.classList.add("form--erro-show");
             formInput.classList.add("form__input__erro--show");
             formWrapperErro.classList.add("form-wrapper__erro--show");
