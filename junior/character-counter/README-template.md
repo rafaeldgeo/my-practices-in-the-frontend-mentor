a5c8de6107e6280605f082e66b26a12a6deef565
Rafael <rfldiasapp@gmail.com>
2025-12-25 18:19:23 -0300

    desenvolvendo contadores

--- junior/character-counter/README-template.md
+++ junior/character-counter/README-template.md
@@ -26,13 +26,10 @@ Users should be able to:
 
 - Analyze the character, word, and sentence counts for their text
 - Exclude/Include spaces in their character count
-- Set a character limit
 - Receive a warning message if their text exceeds their character limit
 - See the approximate reading time of their text
 - Analyze the letter density of their text
-- Select their color theme
-- View the optimal layout for the interface depending on their device's screen size
-- See hover and focus states for all interactive elements on the page
+
 
 ### Screenshot
 
--- junior/character-counter/src/js/analyze.js
+++ junior/character-counter/src/js/analyze.js
@@ -1,14 +1,38 @@
 const textArea = document.querySelector(".form__text");
-const checkBoxes = document.querySelectorAll(".form__checkbox");
+const checkboxes = document.querySelectorAll(".form__checkbox");
 const inputLimit = document.querySelector(".form__input");
-const regEx = /[^0-9]/g;
+const spanTotalCharacters = document.querySelector("#total-characters");
+const spanTotalWords = document.querySelector("#word-count");
+const spanTotalSenteces = document.querySelector("#sentence-count");
 
-let isExcludeSpaces = false;
+const patternLimitInput = /[^0-9]/g;
+const patternExcludeSpaces = /[\s]/g;
+const patternWords = /\p{L}+/gu;
+const patterSentence = /[^.]+\./g;
+let isCheckedExcludeSpaces = false;
 let limitedCharactersDefined;
+let totalCharacters = 0;
 
-// define if the spaces will be excludes
-function excludeSpaces(checkbox) {
-    isExcludeSpaces = checkbox.checked;
+
+// active exclude spaces
+function defineCheckbox(checkbox) {
+    const text = textArea.value;
+    isCheckedExcludeSpaces = checkbox.checked;
+    if (isCheckedExcludeSpaces) {
+        excludeSpaces(text);
+    }
+}
+
+// exclude spaces characters
+function excludeSpaces(text) {
+    let textNoSpaces = text.replace(patternExcludeSpaces, "");
+    countStats(textNoSpaces);
+    showJoinedCharacters(textNoSpaces);
+}
+
+// show the characteres without spaces
+function showJoinedCharacters(textNoSpaces) {
+    textArea.value = textNoSpaces;
 }
 
 // show the input to define limite of characteres
@@ -25,9 +49,13 @@ function showInputLimitCharacters(checkbox) {
     }
 }
 
+// Sanitize input when the user types letter and symbols
+inputLimit.addEventListener("input", () => {
+    inputLimit.value = inputLimit.value.replace(patternLimitInput, "");
+});
+
 // define limit of characters
 function defineLimitCharacters() {
-    inputLimit.value = inputLimit.value.replace(regEx, "");
     const value = Number(inputLimit.value);
     limitedCharactersDefined = value > 0 ? value : undefined;
 }
@@ -37,7 +65,7 @@ function setting(e) {
     const selectedOption = e.target;
     switch (selectedOption.id) {
         case "exclude-spaces":
-            excludeSpaces(selectedOption);
+            defineCheckbox(selectedOption)
             break;
         case "limit-caracters":
             showInputLimitCharacters(selectedOption);
@@ -46,12 +74,44 @@ function setting(e) {
     }
 }
 
-
-checkBoxes.forEach(checkbox => checkbox.addEventListener("change", setting));
-inputLimit.addEventListener("change", defineLimitCharacters);
-
+// input of the caracteres
 function inputText() {
-
+    let textOriginal = textArea.value.trim();
+    if (isCheckedExcludeSpaces) {
+        excludeSpaces(textOriginal);
+        countStats(textOriginal);
+    } else {
+        countStats(textOriginal);
+    }
 }
 
+// count the characters, words and sentences
+function countStats(text) {
+    const totalCharacters = text.length;
+    const totalWords = totalCharacters > 0 ? text.match(patternWords).length : 0;
+    console.log(text.match(patterSentence));
+    const totalSentences = totalCharacters > 0 && text.match(patterSentence) ? text.match(patterSentence).length : 0;
+    showStats(totalCharacters, "characters");
+    showStats(totalWords, "words");
+    showStats(totalSentences, "sentences");
+}
+
+// show the results in the cards
+function showStats(value, typeCount) {
+    switch (typeCount) {
+        case "characters":
+            spanTotalCharacters.textContent = value > 0 ? value.toString().padStart(2, "0") : "00";
+            break;
+        case "words":
+            spanTotalWords.textContent = value > 0 ? value.toString().padStart(2, "0") : "00";
+            break;
+        case "sentences":
+            spanTotalSenteces.textContent = value > 0 ? value.toString().padStart(2, "0") : "00";
+            break;
+        default:
+    }
+}
+
+checkboxes.forEach(checkbox => checkbox.addEventListener("change", setting));
+inputLimit.addEventListener("blur", defineLimitCharacters)
 textArea.addEventListener("input", inputText);
